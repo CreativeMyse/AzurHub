354c642b66f805e6a858551be860e7a54b237a6d
Myse <prog.vale@gmail.com>
2019-03-01 21:15:40 -0500

    Modified files.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+function sleep(ms)
+{
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 var phrases = ["Are you lost?", "Welcome stranger!", "Who are you?"];
 
-$(".menu-content").text(phrases[Math.floor(Math.random() * phrases.length)]);
+async function start()
+{
+    $(".menu-content").text(phrases[Math.floor(Math.random() * phrases.length)]);
+    await sleep(5000);
+    window.location.replace("specials/3dh.html");
+}
+
+start();
